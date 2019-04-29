cea788b3747e9605c914b0ba0f694e68295b7171
gabriel <gdmaher@stanford.edu>
2019-04-28 17:39:03 -0700

    q 11

--- Homework/code_hw3/shift.cuh
+++ Homework/code_hw3/shift.cuh
@@ -1,7 +1,7 @@
 #ifndef _SHIFT_CUH
 #define _SHIFT_CUH
 
-/** 
+/**
  * Repeating from the tutorial, just in case you haven't looked at it.
  * "kernels" or __global__ functions are the entry points to code that executes on the GPU.
  * The keyword __global__ indicates to the compiler that this function is a GPU entry point.
@@ -14,14 +14,17 @@ typedef unsigned int uint;
 
 /**
  * Implements a per-element shift by loading a single byte and shifting it.
- */ 
+ */
 __global__ void shift_char(const uchar *input_array, uchar *output_array,
-                           uchar shift_amount, uint array_length) 
+                           uchar shift_amount, uint array_length)
 {
-    // TODO: fill in
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < array_length){
+      output_array[i] = input_array[i]+shift_amount;
+    }
 }
 
-/** 
+/**
  * Here we load 4 bytes at a time instead of just 1 to improve bandwidth
  * due to a better memory access pattern.
  */
@@ -31,11 +34,11 @@ __global__ void shift_int(const uint *input_array, uint *output_array,
     // TODO: fill in
 }
 
-/** 
+/**
  * Here we go even further and load 8 bytes - does it improve further?
  */
 __global__ void shift_int2(const uint2 *input_array, uint2 *output_array,
-                           uint shift_amount, uint array_length) 
+                           uint shift_amount, uint array_length)
 {
     // TODO: fill in
 }
@@ -44,23 +47,27 @@ __global__ void shift_int2(const uint2 *input_array, uint2 *output_array,
 // and report the time it took for the kernel to run
 
 double doGPUShiftChar(const uchar *d_input, uchar *d_output,
-                      uchar shift_amount, uint text_size, uint block_size) 
+                      uchar shift_amount, uint text_size, uint block_size)
 {
     // TODO: compute your grid dimensions
+    uint num_blocks = text_size/block_size;
+    std::cout << "num blocks " << num_blocks << ", block size " << block_size << "\n";
+    std::cout << "shift amount " << static_cast<unsigned>(shift_amount) << "\n";
 
     // TODO: compute 4 byte shift value
-
     event_pair timer;
     start_timer(&timer);
 
     // TODO: launch kernel
+    shift_char<<<num_blocks, block_size>>>(d_input, d_output,
+        shift_amount, text_size);
 
     check_launch("gpu shift cipher uint");
     return stop_timer(&timer);
 }
 
 double doGPUShiftUInt(const uchar *d_input, uchar *d_output,
-                      uchar shift_amount, uint text_size, uint block_size) 
+                      uchar shift_amount, uint text_size, uint block_size)
 {
     // TODO: compute grid dimensions
 
@@ -70,13 +77,13 @@ double doGPUShiftUInt(const uchar *d_input, uchar *d_output,
     start_timer(&timer);
 
     // TODO: launch kernel
-    
+
     check_launch("gpu shift cipher uint");
     return stop_timer(&timer);
 }
 
 double doGPUShiftUInt2(const uchar* d_input, uchar* d_output,
-                       uchar shift_amount, uint text_size, uint block_size) 
+                       uchar shift_amount, uint text_size, uint block_size)
 {
     // TODO: compute your grid dimensions
 
