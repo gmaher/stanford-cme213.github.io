c253d03be92ec228b11dba07c5db71800c8f5f8f
gabriel <gdmaher@stanford.edu>
2019-06-01 08:42:46 -0700

    moving nn class

--- Homework/code_project/starter_code/inc/gpu_func.h
+++ Homework/code_project/starter_code/inc/gpu_func.h
@@ -5,51 +5,6 @@
 #include <helper_cuda.h>
 #include <helper_functions.h>
 
-struct event_pair {
-    cudaEvent_t start;
-    cudaEvent_t end;
-};
-
-inline void check_launch(const char* kernel_name) {
-    cudaDeviceSynchronize();
-    cudaError_t err = cudaGetLastError();
-
-    if(err != cudaSuccess) {
-        std::cerr << "error in " << kernel_name << " kernel" << std::endl;
-        std::cerr << "error was: " << cudaGetErrorString(err) << std::endl;
-        exit(1);
-    }
-}
-
-inline void start_timer(event_pair* p) {
-    cudaEventCreate(&p->start);
-    cudaEventCreate(&p->end);
-    cudaEventRecord(p->start, 0);
-}
-
-
-inline double stop_timer(event_pair* p) {
-    cudaEventRecord(p->end, 0);
-    cudaEventSynchronize(p->end);
-
-    float elapsed_time;
-    cudaEventElapsedTime(&elapsed_time, p->start, p->end);
-    cudaEventDestroy(p->start);
-    cudaEventDestroy(p->end);
-    return elapsed_time;
-}
-
-int useless_gpu_add_one(int t);
-
-int myGEMM(double* A, double* B, double* C, double* alpha, double* beta, int M,
-           int N, int K);
-
-int mySigmoid(double* S, double* X, int M, int N);
-int myHadamard(double* X, double* Y, double* H, int M, int N);
-int myTranspose(double* X, double* Xt, int M, int N);
-int myMatAdd(double* X, double* Y, double* Z, int M, int N, double alpha);
-int mySoftmax(double* X, double* S, int M, int N);
-
 class NeuralNetworkGPU {
 public:
   int n_feats;
@@ -99,4 +54,50 @@ public:
     }
 
 };
+
+struct event_pair {
+    cudaEvent_t start;
+    cudaEvent_t end;
+};
+
+inline void check_launch(const char* kernel_name) {
+    cudaDeviceSynchronize();
+    cudaError_t err = cudaGetLastError();
+
+    if(err != cudaSuccess) {
+        std::cerr << "error in " << kernel_name << " kernel" << std::endl;
+        std::cerr << "error was: " << cudaGetErrorString(err) << std::endl;
+        exit(1);
+    }
+}
+
+inline void start_timer(event_pair* p) {
+    cudaEventCreate(&p->start);
+    cudaEventCreate(&p->end);
+    cudaEventRecord(p->start, 0);
+}
+
+
+inline double stop_timer(event_pair* p) {
+    cudaEventRecord(p->end, 0);
+    cudaEventSynchronize(p->end);
+
+    float elapsed_time;
+    cudaEventElapsedTime(&elapsed_time, p->start, p->end);
+    cudaEventDestroy(p->start);
+    cudaEventDestroy(p->end);
+    return elapsed_time;
+}
+
+int useless_gpu_add_one(int t);
+
+int myGEMM(double* A, double* B, double* C, double* alpha, double* beta, int M,
+           int N, int K);
+
+int mySigmoid(double* S, double* X, int M, int N);
+int myHadamard(double* X, double* Y, double* H, int M, int N);
+int myTranspose(double* X, double* Xt, int M, int N);
+int myMatAdd(double* X, double* Y, double* Z, int M, int N, double alpha);
+int mySoftmax(double* X, double* S, int M, int N);
+
 #endif
