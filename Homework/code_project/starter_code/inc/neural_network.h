fb5dfbeeec530714fca7ab1bbc0acfb635fcfb7f
gabriel <gdmaher@stanford.edu>
2019-06-01 08:29:21 -0700

    nngpu 2

--- Homework/code_project/starter_code/inc/neural_network.h
+++ Homework/code_project/starter_code/inc/neural_network.h
@@ -48,53 +48,5 @@ void parallel_train(NeuralNetwork& nn, const arma::mat& X, const arma::mat& y,
                     const int batch_size = 800, bool grad_check = false, int print_every = -1,
                     int debug = 0);
 
-class NeuralNetworkGPU {
-public:
-
-  int n_feats;
-  int n_classes;
-  int n_batch;
-
-  double* Xd;
-  double* Yd;
-
-  double* W1_d;
-  double* b1_d;
-  double* z1_d;
-  double* a1_d;
-
-  double* W2_d;
-  double* b2_d;
-  double* z2_d;
-  double* a2_d;
-
-  double* yhat_d;
-    NeuralNetworkGPU(NeuralNetwork nn, int x_size, int y_size, int batch_size) {
-      n_feats = x_size;
-      n_classes = y_size;
-      n_batch = batch_size;
-
-      cudaMalloc((void**)&Xd, sizeof(double)*x_size*batch_size);
-      cudaMalloc((void**)&Yd, sizeof(double)*y_size*y_size);
-
-      int h1 = nn.W[0].n_rows;
-      int w1 = nn.W[0].n_cols;
-      cudaMalloc((void**)&W1_d, sizeof(double)*h1*w1);
-      cudaMalloc((void**)&b1_d, sizeof(double)*h1);
-      cudaMalloc((void**)&z1_d, sizeof(double)*h1*batch_size);
-      cudaMalloc((void**)&a1_d, sizeof(double)*h1*batch_size);
-      std::cout << "layer 1 h=" << h1 << ", w=" << w1 << "\n";
-
-      int h2 = nn.W[1].n_rows;
-      int w2 = nn.W[1].n_cols;
-      cudaMalloc((void**)&W2_d, sizeof(double)*h2*w2);
-      cudaMalloc((void**)&b2_d, sizeof(double)*h2);
-      cudaMalloc((void**)&z2_d, sizeof(double)*h2*batch_size);
-      cudaMalloc((void**)&a2_d, sizeof(double)*h2*batch_size);
-      std::cout << "layer 2 h=" << h2 << ", w=" << w2 << "\n";
-    }
-
-};
-
 
 #endif
