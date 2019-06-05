5d98ecf6e59ff2991c7a50d9a3ec06295e8bc948
gabriel <gdmaher@stanford.edu>
2019-06-05 14:08:48 -0700

    a

--- Homework/code_project/starter_code/inc/gpu_func.h
+++ Homework/code_project/starter_code/inc/gpu_func.h
@@ -237,7 +237,7 @@ int rank;
     cudaMemcpy(db1_h, db1, sizeof(double)*n_hidden*n_batch, cudaMemcpyDeviceToHost);
     cudaMemcpy(dW2_h, dW2, sizeof(double)*n_classes*n_hidden, cudaMemcpyDeviceToHost);
     cudaMemcpy(db2_h, db2, sizeof(double)*n_classes*n_batch, cudaMemcpyDeviceToHost);
-    //std::cout << "db1_h " << db1_h[0] << "\n";
+    std::cout << rank << " db1_h " << db1_h[0] << "\n";
   }
 
   void gradientToDevice(){
